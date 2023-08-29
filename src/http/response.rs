9026e1b727cc6269c569bfc72025d5ec8b9efb61
Gokul Ganesh I <igokulganesh@gmail.com>
2023-08-29 22:59:53 +0530

    Static Response added

--- src/http/response.rs
+++ src/http/response.rs
@@ -1,4 +1,5 @@
 use super::StatusCode;
+use std::fmt::{Display, Formatter, Result as FmtResult};
 
 
 #[derive(Debug)]
@@ -10,5 +11,17 @@ pub struct Response {
 impl Response {
     pub fn new(status_code: StatusCode, body: Option<String>) -> Self {
         Response { status_code, body }
+    }
+}
+
+impl Display for Response {
+    fn fmt(&self, f: &mut Formatter<'_>) -> FmtResult {
+
+        let body = match &self.body {
+            Some(b) => b,
+            None => "",
+        };
+
+        write!(f, "HTTP/1.1 {} {}\r\n\r\n{}", self.status_code, self.status_code.details(), body)
     }
 }
--- src/server.rs
+++ src/server.rs
@@ -1,6 +1,6 @@
 use std::net::TcpListener;
-use std::io::Read;
-use crate::http::Request;
+use std::io::{Read, Write};
+use crate::http::{Request, Response, StatusCode};
 
 
 pub struct Server {
@@ -28,6 +28,8 @@ impl Server {
                             match Request::try_from(&buffer[..]) {
                                 Ok(request) => {
                                     dbg!(request);
+                                    let response = Response::new(StatusCode::Ok, Some("<h1>Hello World!</h1>".to_string()));
+                                    write!(stream, "{}", response).unwrap();
                                 },
                                 Err(e) => println!("Failed to Parse the Request: {}", e),
                             }
