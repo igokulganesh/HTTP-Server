e82aa32b48b3806f0811ccda069d4f930f581802
Gokul Ganesh I <igokulganesh@gmail.com>
2023-08-30 22:12:28 +0530

    Added Dynamic Response

--- src/http/request_handler.rs
+++ src/http/request_handler.rs
@@ -1,4 +1,4 @@
-use super::{Request, Response, ParseError, StatusCode};
+use super::{Request, Response, ParseError, StatusCode, Method};
 
 pub trait Handler {
     fn handle_request(&mut self, request: &Request) -> Response; 
@@ -13,6 +13,14 @@ pub struct WebsiteHandler;
 
 impl Handler for WebsiteHandler {
     fn handle_request(&mut self, request: &Request) -> Response {
-        Response::new(StatusCode::Ok, Some("<h1>It works</h1>".to_string())) 
+
+        match request.method() {
+            Method::GET => match request.path() {
+                    "/" => Response::new(StatusCode::Ok, Some("<h1>Welcome</h1>".to_string())),
+                    "/about" => Response::new(StatusCode::Ok, Some("<h1>Hello World!</h1>".to_string())),
+                    _ => Response::new(StatusCode::NotFound, Some("<h1>Page Not Found 404</h1>".to_string())), 
+                },
+            _ => Response::new(StatusCode::NotFound, Some("<h1>Page Not Found 404</h1>".to_string())),
+        }
     }
 }
